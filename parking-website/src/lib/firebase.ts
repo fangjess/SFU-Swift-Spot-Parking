e61769ea91e2f5292713106945a77e9f2a9b251b
Jessica Fang <140139367+fangjess@users.noreply.github.com>
2024-10-05 19:03:37 -0700

    types script and firebae integration

---
+++ parking-website/src/lib/firebase.ts
@@ -0,0 +1,32 @@
+import { initializeApp } from "firebase/app";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDfVpJmNgC0Yt7xcxxpPHwXIrMbopzUE1s",
+  authDomain: "stormhacks-2024-6f854.firebaseapp.com",
+  projectId: "stormhacks-2024-6f854",
+  storageBucket: "stormhacks-2024-6f854.appspot.com",
+  messagingSenderId: "360173476909",
+  appId: "1:360173476909:web:658c2a62542b3fe984aca4",
+  measurementId: "G-65CXNZ8862"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+export async function getAllParking(){
+    const docs = await getDocs(collection(db, "parkings"));
+    docs.forEach(item=>{
+        console.log(item.data());
+        
+    })
+
+}
--- parking-website/src/routes/+page.svelte
+++ parking-website/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { getAllParking } from "$lib/firebase";
+
     // You can add any reactive data here
     let title = "SFU Parking Services";
     let entryfileds = "Please enter the following fields:";
@@ -18,7 +20,7 @@
         North: { ratings: [], mode: null },
         East: { ratings: [], mode: null },
         West: { ratings: [], mode: null },
-        South: { ratings: [], mode: null}
+        South: { ratings: [], mode: null },
     };
 
     // Function to handle new ratings for the selected lot
@@ -48,139 +50,124 @@
             }
         });
         if (mode == 1) {
-          mode = "Empty";
+            mode = "Empty";
         } else if (mode == 2) {
-          mode = "Partially Empty";
+            mode = "Partially Empty";
         } else if (mode == 3) {
-          mode = "Half Full";
+            mode = "Half Full";
         } else if (mode == 4) {
-          mode = "Partially Full";
+            mode = "Partially Full";
         } else {
-          mode = "Full";
+            mode = "Full";
         }
         lots[lot].mode = mode;
     }
 
     const handleSubmit = () => {
-    submittedPlate = licensePlate; // Store the entered license plate
-    submittedLot = lot; 
-    submittedstartTime = startTime;
-    submittedendTime = endTime;
-    submittedlotFullness = lotFullness;
-    submittedtypeParking = typeParking;
-    //alert(`License Plate Submitted: ${submittedPlate, submittedLot}`);
-    submitRating();
+        submittedPlate = licensePlate; // Store the entered license plate
+        submittedLot = lot;
+        submittedstartTime = startTime;
+        submittedendTime = endTime;
+        submittedlotFullness = lotFullness;
+        submittedtypeParking = typeParking;
+        //alert(`License Plate Submitted: ${submittedPlate, submittedLot}`);
+        submitRating();
     };
 
-  </script>
-  
-  <style>
-    /* Add some basic styling */
-    main {
-      font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-      background-color: #f9f9f9;
-      margin: 0;
-      padding: 8rem;
-      text-align: center;
+    function test() {
+        getAllParking();
     }
-  
-    header {
-      background-color: #CC0633 ;
-      color: white;
-      padding: 1rem;
-      text-align: center;
-    }
+</script>
 
-    button {
-    padding: 0.1rem 1rem;
-    font-size: 1rem;
-    background-color: #4CAF50;
-    color: white;
-    border: none;
-    cursor: pointer;
-    }
 
-    button:hover {
-    background-color: #45a049;
-    }
-  
-    footer {
-      background-color: #CC0633 ;
-      color: white;
-      text-align: center;
-      padding: 1rem;
-      bottom: 0;
-      width: 90%;
-    }
-  </style>
-  
-  <!-- Structure of the page -->
-  <header>
+<button on:click={()=>{
+    test()
+}}>CLICK ME</button>
+<!-- Structure of the page -->
+<header>
     <h1>{title}</h1>
-  </header>
-  
-  
-  <main>
+</header>
+
+<main>
     <h3>Current Lot Fullness</h3>
-      <p>North: {lots.North.mode !== null ? lots.North.mode : 'No data yet'}</p>
-      <p>East: {lots.East.mode !== null ? lots.East.mode : 'No data yet'}</p>
-      <p>West: {lots.West.mode !== null ? lots.West.mode : 'No data yet'}</p>
-      <p>South: {lots.South.mode !== null ? lots.South.mode : 'No data yet'}</p>
+    <p>North: {lots.North.mode !== null ? lots.North.mode : "No data yet"}</p>
+    <p>East: {lots.East.mode !== null ? lots.East.mode : "No data yet"}</p>
+    <p>West: {lots.West.mode !== null ? lots.West.mode : "No data yet"}</p>
+    <p>South: {lots.South.mode !== null ? lots.South.mode : "No data yet"}</p>
     <p>{entryfileds}</p>
     <div>
-        <label 
-        for="lot">Parking lot:
-        </label>
-        <select name="lot" id="lot" bind:value={lot} >
-          <option value="North">North Lot</option>
-          <option value="East">East Lot</option>
-          <option value="South">South Lot</option>
-          <option value="West">West Parkade</option>
+        <label for="lot">Parking lot: </label>
+        <select name="lot" id="lot" bind:value={lot}>
+            <option value="North">North Lot</option>
+            <option value="East">East Lot</option>
+            <option value="South">South Lot</option>
+            <option value="West">West Parkade</option>
         </select>
-        <label 
-        for="start">Start time:
-        </label>
-        <input 
-        type="time" 
-        id="start" 
-        name="start"
-        bind:value={startTime}
-        />
-        <label 
-        for="start">End time:
-       </label>
-        <input 
-        type="time" 
-        id="end" 
-        name="end"
-        bind:value={endTime}
-        />
-        <label 
-        for="fullness">Lot fullness:
-       </label>
+        <label for="start">Start time: </label>
+        <input type="time" id="start" name="start" bind:value={startTime} />
+        <label for="start">End time: </label>
+        <input type="time" id="end" name="end" bind:value={endTime} />
+        <label for="fullness">Lot fullness: </label>
         <select name="fullness" id="fullness" bind:value={lotFullness}>
-          <option value="1">1 - Empty</option>
-          <option value="2">2 - Partially Empty</option>
-          <option value="3">3 - Half Full</option>
-          <option value="4">4 - Partially Full</option>
-          <option value="5">5 - Full</option>
+            <option value="1">1 - Empty</option>
+            <option value="2">2 - Partially Empty</option>
+            <option value="3">3 - Half Full</option>
+            <option value="4">4 - Partially Full</option>
+            <option value="5">5 - Full</option>
         </select>
 
         <button on:click={handleSubmit}>Submit</button>
     </div>
-    
-    
-    
-    {#if submittedLot, submittedstartTime, submittedendTime, submittedlotFullness}
-    <p>Your lot: {submittedLot}</p>
-    <p>Your start time: {submittedstartTime}</p>
-    <p>Your end time: {submittedendTime}</p>
-    <p>Your lot fullness: {submittedlotFullness}</p>
+
+    {#if (submittedLot, submittedstartTime, submittedendTime, submittedlotFullness)}
+        <p>Your lot: {submittedLot}</p>
+        <p>Your start time: {submittedstartTime}</p>
+        <p>Your end time: {submittedendTime}</p>
+        <p>Your lot fullness: {submittedlotFullness}</p>
     {/if}
-    
-  </main>
-  
-  <footer>
+</main>
+
+<footer>
     <p>&copy; 2024 Jessica Emily Gursimrit Paria. All rights reserved.</p>
-  </footer>
-  
+</footer>
+
+<style>
+    /* Add some basic styling */
+    main {
+        font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
+            sans-serif;
+        background-color: #f9f9f9;
+        margin: 0;
+        padding: 8rem;
+        text-align: center;
+    }
+
+    header {
+        background-color: #cc0633;
+        color: white;
+        padding: 1rem;
+        text-align: center;
+    }
+
+    button {
+        padding: 0.1rem 1rem;
+        font-size: 1rem;
+        background-color: #4caf50;
+        color: white;
+        border: none;
+        cursor: pointer;
+    }
+
+    button:hover {
+        background-color: #45a049;
+    }
+
+    footer {
+        background-color: #cc0633;
+        color: white;
+        text-align: center;
+        padding: 1rem;
+        bottom: 0;
+        width: 90%;
+    }
+</style>
